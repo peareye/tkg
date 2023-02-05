6d48d4f1ffe93be14a4a885819a0c9ada97d1737
peri.moritz@gmail.com <>
2023-02-04 18:02:10 -0800

    updated custom.js file for the carousel.

--- public/assets/default/js/custom.js
+++ public/assets/default/js/custom.js
@@ -3,21 +3,34 @@
 // Custom Slider/Carousel 
 const track = document.querySelector(".slider__track");
 const slides = Array.from(track.children);
+
 const nextButton = document.querySelector(".slider__button--right");
 const prevButton = document.querySelector(".slider__button--left");
 const dotsNav = document.querySelector(".slider__nav");
 const dots = Array.from(dotsNav.children);
+const sliderContainer = document.querySelector('.slider__track-container');
 
 const slideWidth = slides[0].getBoundingClientRect().width;
+
+// Capture max height of inner carousel slide
+let maxSlideHeight = 0;
 
 //FUNCTIONS
 
 // Arrange slides next to each other
 const setSlidePosition = (slide, index) => {
   slide.style.left = slideWidth * index + "px";
+
+// Set Height of tallest slide to default slide height
+  let ht = slide.querySelector('.slider__slide--inner').getBoundingClientRect().height;
+  if (ht > maxSlideHeight) {
+   maxSlideHeight = ht;
+  }
 };
 
 slides.forEach(setSlidePosition);
+
+sliderContainer.style.height = maxSlideHeight + "px";
 
 // Function for left/right button
 const moveToSlide = (track, currentSlide, targetSlide) => {
@@ -30,20 +43,6 @@ const updateDots = (currentDot, targetDot) => {
   currentDot.classList.remove("current-slide");
   targetDot.classList.add("current-slide");
 };
-
-const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
-  if (targetIndex == 0) {
-    prevButton.classList.add("is-hidden");
-    nextButton.classList.remove("is-hidden");
-  } else if (targetIndex === slides.length - 1) {
-    prevButton.classList.remove("is-hidden");
-    nextButton.classList.add("is-hidden");
-  } else {
-    prevButton.classList.remove("is-hidden");
-    nextButton.classList.remove("is-hidden");
-  }
-};
-
 
 // when I click nav indicator move to that slide
 dotsNav.addEventListener("click", (e) => {
@@ -59,32 +58,4 @@ dotsNav.addEventListener("click", (e) => {
 
   moveToSlide(track, currentSlide, targetSlide);
   updateDots(currentDot, targetDot);
-  hideShowArrows(slides, prevButton, nextButton, targetIndex);
 });
-
-
-// When I click Left, move slides to the left
-// prevButton.addEventListener("click", (e) => {
-//   const currentSlide = track.querySelector(".current-slide");
-//   const prevSlide = currentSlide.previousElementSibling;
-//   const currentDot = dotsNav.querySelector(".current-slide");
-//   const prevDot = currentDot.previousElementSibling;
-//   const prevIndex = slides.findIndex((slide) => slide === prevSlide);
-
-//   moveToSlide(track, currentSlide, prevSlide);
-//   updateDots(currentDot, prevDot);
-//   hideShowArrows(slides, prevButton, nextButton, prevIndex);
-// });
-
-// When I click Right, move slides to the right
-// nextButton.addEventListener("click", (e) => {
-//   const currentSlide = track.querySelector(".current-slide");
-//   const nextSlide = currentSlide.nextElementSibling;
-//   const currentDot = dotsNav.querySelector(".current-slide");
-//   const nextDot = currentDot.nextElementSibling;
-//   const nextIndex = slides.findIndex((slide) => slide === nextSlide);
-
-//   moveToSlide(track, currentSlide, nextSlide);
-//   updateDots(currentDot, nextDot);
-//   hideShowArrows(slides, prevButton, nextButton, nextIndex);
-// });
